ef6cf2681d3485d7bf668fc29acc2e2ad19faed4
josue <josuefig19@gmail.com>
2025-08-17 21:55:40 -0600

    Agrega la lección de named imports y namespace imports

--- Modulos/public/bundle.js
+++ Modulos/public/bundle.js
@@ -1,9 +1,43 @@
 'use strict';
 
-var carrito = () => {
-    console.log('Carrito');
-    
+/*
+	📌 Named Exports
+	
+	Podemos exportar variables y funciones utilizando named exports o default exports.
+	Cuando trabajamos con named exports podemos exportar multiples cosas.
+	Podemos exportar de dos formas:
+	- Agregando la palabra export antes de la variable.
+	- Al final de documento
+*/
+
+// Forma 1 - Palabra export
+const nombre = 'Carlos Arturo';
+
+const obtenerPosts = () => {
+	return ['Post1', 'Post2', 'Post3'];
 };
 
-console.log('Archivo inicial');
-carrito();
+// Forma 2 - Final del documento
+// const nombre = 'Carlos Arturo';
+
+// const obtenerPosts = () => {
+// 	return ['Post1', 'Post2', 'Post3'];
+// };
+
+// export { nombre, obtenerPosts };
+
+/* 
+	📌 Named Imports
+*/
+/* 
+	📌 Named Imports
+	Podemos importar variables y funciones que han sido exportadas mediante la palabra "export".
+	Podemos importarlas con el mismo nombre o podemos usar un alias usando la palabra "as".
+	Normalmente todos los import se establecen al inicio del documento.
+*/
+// import { nombre as nombreImportado, obtenerPosts } from './namedExports';
+// console.log('Mi nombre es ' + nombreImportado);
+// console.log(obtenerPosts());
+
+console.log(nombre);
+console.log(obtenerPosts());
--- Modulos/src/index.js
+++ Modulos/src/index.js
@@ -1,4 +1,23 @@
-import carrito from './carrito';
+/* 
+	📌 Named Imports
+*/
+/* 
+	📌 Named Imports
+	Podemos importar variables y funciones que han sido exportadas mediante la palabra "export".
+	Podemos importarlas con el mismo nombre o podemos usar un alias usando la palabra "as".
+	Normalmente todos los import se establecen al inicio del documento.
+*/
+// import { nombre as nombreImportado, obtenerPosts } from './namedExports';
+// console.log('Mi nombre es ' + nombreImportado);
+// console.log(obtenerPosts());
 
-console.log('Archivo inicial');
-carrito();
+/* 
+	📌 Namespace Imports
+*/
+/* 
+	Podemos importar todas las variables y funciones que fueron exportadas 
+	con un named export dentro de un objeto.
+*/
+import * as datos from './namedExports';
+console.log(datos.nombre);
+console.log(datos.obtenerPosts());
