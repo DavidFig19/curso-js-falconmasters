e7a3376f26ba7d7134b8b4c69b071aaf41dcecb9
josue <josuefig19@gmail.com>
2025-10-06 10:06:05 -0600

    Agrega el borde al slide activo

--- Proyectos/Galeria/src/galeria/cargarImagen.js
+++ Proyectos/Galeria/src/galeria/cargarImagen.js
@@ -1,3 +1,5 @@
+import data from './../datos/fotos';
+
 const galeria = document.getElementById('galeria');
 
 const cargarImagen = (id, nombre, ruta, descripcion) => {
@@ -5,6 +7,29 @@ const cargarImagen = (id, nombre, ruta, descripcion) => {
     galeria.querySelector('.galeria__imagen').dataset.idImagen = id;
     galeria.querySelector('.galeria__titulo').innerText = nombre;
     galeria.querySelector('.galeria__descripcion-imagen-activa').innerText = descripcion;
+
+    const categoriaActual = galeria.dataset.categoria;
+    const fotos = data.fotos[categoriaActual];
+    let indexImagenActual;
+    fotos.forEach((foto, index) => {
+        if(foto.id === id){
+           indexImagenActual = index;
+        }
+    });
+
+  
+    //Marcados la imagne del carousel como activa
+    if(galeria.querySelectorAll('.galeria__carousel-slide').length > 0){
+
+        //Eliminamos la clase active de cualquier slide que la tenga
+        galeria.querySelector('.galeria__carousel-slide--active').classList.remove('galeria__carousel-slide--active');
+
+
+        galeria.querySelectorAll('.galeria__carousel-slide')[indexImagenActual].classList.add('galeria__carousel-slide--active');
+    }
+   
+   
+    
 }
 
 export {cargarImagen};
