c6703eb459f19dcf4ec047aaa1c7bdb97e7b01f3
josue <josuefig19@gmail.com>
2025-11-21 16:31:08 -0600

    Agregando funcion para cambiar thumbs del producto

---
+++ Proyectos/Tienda/src/producto.js
@@ -0,0 +1,19 @@
+const producto = document.getElementById('producto');
+const productoImagen = producto.querySelector('.producto__imagen');
+const thumbs = producto.querySelector('.producto__thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  if(e.target.tagName === 'IMG'){
+    const imagenSrc = e.target.src;
+
+    //obtenemos la posicion del ultimo / 
+    const lastIndex = imagenSrc.lastIndexOf('/');
+
+    //cortamos la cadena de texto para obtener solamente una parte.
+    const nombreImagen = imagenSrc.substring(lastIndex + 1);
+    
+    //Cambiamos la ruta de la imagen del producto.
+    productoImagen.src = `./img/tennis/${nombreImagen}`;
+  }
+  
+});
