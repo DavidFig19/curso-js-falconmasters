6cf69c57b47e2feb12c14145a899e0f270d07b59
josue <josuefig19@gmail.com>
2025-11-24 12:08:37 -0600

    Agrega suma del total de los productos en la ventana del carrito

--- Proyectos/Tienda/src/carrito.js
+++ Proyectos/Tienda/src/carrito.js
@@ -1,4 +1,4 @@
-import data from './data/productos';
+import data from "./data/productos";
 const botonesAbrirCarrito = document.querySelectorAll(
   '[data-accion="abrir-carrito"]'
 );
@@ -9,84 +9,106 @@ const ventanaCarrito = document.getElementById("carrito");
 const btnAgregarCarrito = document.getElementById("agregar-al-carrito");
 const producto = document.getElementById("producto");
 let carrito = [];
-const formatearMoneda = new Intl.NumberFormat('es-MX',{style: 'currency', currency:'MXN'});
+const formatearMoneda = new Intl.NumberFormat("es-MX", {
+  style: "currency",
+  currency: "MXN",
+});
 
 const renderCarrito = () => {
   ventanaCarrito.classList.add("carrito--active");
 
   // Eliminamos todos los productos anteriores para construir el carrito desde cero.
-  const productosAnteriores = ventanaCarrito.querySelectorAll('.carrito__producto');
-  productosAnteriores.forEach(producto => producto.remove());
+  const productosAnteriores =
+    ventanaCarrito.querySelectorAll(".carrito__producto");
+  productosAnteriores.forEach((producto) => producto.remove());
 
-  // Itereamos sobre cada producto del carrito y lo mostramos
-  carrito.forEach((productoCarrito) => {
+  let total = 0;
 
-    // Obtenemos el precio del archivo de producto.js
-    // Cuando el id del item del carrito sea el mismo que alguno de la lista.
-    data.productos.forEach((ProductoBaseDatos) => {
-      if(ProductoBaseDatos.id === productoCarrito.id){
-        productoCarrito.precio = ProductoBaseDatos.precio;
+  // Comprobamos si hay productos
+  if (carrito.length < 1) {
+    // Ponemos la clase de carrito vacio
+    ventanaCarrito.classList.add("carrito--vacio");
+  } else {
+
+    ventanaCarrito.classList.remove("carrito--vacio");
+
+    // Itereamos sobre cada producto del carrito y lo mostramos
+    carrito.forEach((productoCarrito) => {
+
+      // Obtenemos el precio del archivo de producto.js
+      // Cuando el id del item del carrito sea el mismo que alguno de la lista.
+      data.productos.forEach((ProductoBaseDatos) => {
+        if (ProductoBaseDatos.id === productoCarrito.id) {
+          productoCarrito.precio = ProductoBaseDatos.precio;
+
+          total += productoCarrito.precio * productoCarrito.cantidad;
+          
+        }
+      });
+
+      // Establecemos la ruta de la imagen que vamos a querer mostrar.
+      let thumbSrc = producto.querySelectorAll(".producto__thumb-img")[0].src;
+      if (productoCarrito.color === "rojo") {
+        thumbSrc = "./img/thumbs/rojo.jpg";
+      } else if (productoCarrito.color === "amarillo") {
+        thumbSrc = "./img/thumbs/amarillo.jpg";
       }
+
+      // creamos una plantilla del codigo HTML.
+      const plantillaProducto = `
+      <div class="carrito__producto-info">
+        <img src="${thumbSrc}" alt="" class="carrito__thumb" />
+        <div>
+          <p class="carrito__producto-nombre">
+            <span class="carrito__producto-cantidad">${
+              productoCarrito.cantidad
+            } x </span> ${productoCarrito.nombre}
+          </p>
+          <p class="carrito__producto-propiedades">
+            Tamaño:<span>${productoCarrito.tamaño}</span> Color:<span>${
+          productoCarrito.color
+        }</span>
+          </p>
+        </div>
+      </div>
+      <div class="carrito__producto-contenedor-precio">
+        <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+          <path
+            d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+          />
+          </svg>
+        </button>
+        <p class="carrito__producto-precio">
+          ${formatearMoneda.format(
+            productoCarrito.precio * productoCarrito.cantidad
+          )}
+        </p>
+      </div>
+      `;
+
+      //  Creamos un div
+      const itemCarrito = document.createElement("div");
+
+      // Le agregamos la clase de carrito__producto.
+      itemCarrito.classList.add("carrito__producto");
+
+      // Le insertamos la plantilla dentro del elemento
+      itemCarrito.innerHTML = plantillaProducto;
+
+      //  Agregamos el producto a la ventana del carrito.
+      ventanaCarrito.querySelector(".carrito__body").appendChild(itemCarrito);
     });
 
+  }
 
-    // Establecemos la ruta de la imagen que vamos a querer mostrar.
-    let thumbSrc = producto.querySelectorAll('.producto__thumb-img')[0].src;
-    if(productoCarrito.color === 'rojo'){
-      thumbSrc = './img/thumbs/rojo.jpg';
-    }else if(productoCarrito.color === 'amarillo'){
-      thumbSrc = './img/thumbs/amarillo.jpg';
-    }
-  
-
-    // creamos una plantilla del codigo HTML.
-    const plantillaProducto = `
-    <div class="carrito__producto-info">
-			<img src="${thumbSrc}" alt="" class="carrito__thumb" />
-			<div>
-				<p class="carrito__producto-nombre">
-					<span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span> ${productoCarrito.nombre}
-				</p>
-				<p class="carrito__producto-propiedades">
-					Tamaño:<span>${productoCarrito.tamaño}</span> Color:<span>${productoCarrito.color}</span>
-				</p>
-			</div>
-		</div>
-		<div class="carrito__producto-contenedor-precio">
-			<button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="16"
-					height="16"
-					fill="currentColor"
-					viewBox="0 0 16 16"
-				>
-				<path
-					d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
-				/>
-				</svg>
-			</button>
-			<p class="carrito__producto-precio">
-        ${
-          formatearMoneda.format(productoCarrito.precio  * productoCarrito.cantidad)
-        }
-      </p>
-		</div>
-    `;
-
-    //  Creamos un div
-    const itemCarrito = document.createElement("div");
-
-    // Le agregamos la clase de carrito__producto.
-    itemCarrito.classList.add('carrito__producto');
-
-    // Le insertamos la plantilla dentro del elemento
-    itemCarrito.innerHTML = plantillaProducto;
-
-    //  Agregamos el producto a la ventana del carrito.
-    ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
-
-  });
+  ventanaCarrito.querySelector('.carrito__total').innerText = formatearMoneda.format(total);
 };
 
 // Abrir carrito
@@ -112,17 +134,22 @@ btnAgregarCarrito.addEventListener("click", () => {
     "#propiedad-tamaño input:checked"
   ).value;
 
-  if(carrito.length > 0){
+  if (carrito.length > 0) {
     let productoEnCarrito = false;
 
-    carrito.forEach(item => {
-      if(item.id === id && item.nombre === nombre && item.color === color && item.tamaño === tamaño){
+    carrito.forEach((item) => {
+      if (
+        item.id === id &&
+        item.nombre === nombre &&
+        item.color === color &&
+        item.tamaño === tamaño
+      ) {
         item.cantidad += cantidad;
         productoEnCarrito = true;
       }
     });
 
-    if(!productoEnCarrito){
+    if (!productoEnCarrito) {
       carrito.push({
         id: id,
         nombre: nombre,
@@ -131,8 +158,7 @@ btnAgregarCarrito.addEventListener("click", () => {
         tamaño: tamaño,
       });
     }
-
-  }else{
+  } else {
     carrito.push({
       id: id,
       nombre: nombre,
@@ -141,27 +167,22 @@ btnAgregarCarrito.addEventListener("click", () => {
       tamaño: tamaño,
     });
   }
-  
 });
 
-
 // Botones eliminar del carrito
-ventanaCarrito.addEventListener('click', (e) =>{
-  if(e.target.closest('button')?.dataset.accion === 'eliminar-item-carrito'){
-    const producto = e.target.closest('.carrito__producto');
-    const indexProducto = [...ventanaCarrito.querySelectorAll('.carrito__producto')].indexOf(producto);
-    
+ventanaCarrito.addEventListener("click", (e) => {
+  if (e.target.closest("button")?.dataset.accion === "eliminar-item-carrito") {
+    const producto = e.target.closest(".carrito__producto");
+    const indexProducto = [
+      ...ventanaCarrito.querySelectorAll(".carrito__producto"),
+    ].indexOf(producto);
 
-    carrito = carrito.filter((item,index) => {
-      if(index !== indexProducto){
+    carrito = carrito.filter((item, index) => {
+      if (index !== indexProducto) {
         return item;
       }
-
     });
-    
-    renderCarrito();
-    
-    
-  }
 
+    renderCarrito();
+  }
 });
