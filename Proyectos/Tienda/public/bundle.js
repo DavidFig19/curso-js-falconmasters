ee0e725d75e636a133ddcb24ea1863b57b1e3c9c
josue <josuefig19@gmail.com>
2025-11-25 09:40:05 -0600

    Agrega funcionalidad al proyecto de las tabs

--- Proyectos/Tienda/public/bundle.js
+++ Proyectos/Tienda/public/bundle.js
@@ -285,3 +285,37 @@ ventanaCarrito.querySelector('#carrito__btn-comprar').addEventListener('click', 
   
 
 });
+
+class Tabs {
+    constructor(idElemento){
+        this.tabs = document.getElementById(idElemento);
+        this.nav = this.tabs.querySelector('.tabs');
+
+        // Comprobamos que el elemento que clickeamos tenga la clase de tabs__link.
+        this.nav.addEventListener('click', (e)=> {
+            if([...e.target.classList].includes('tabs__button')){
+                // Obtenemos la tab que queremos mostrar.
+                const tab = e.target.dataset.tab;
+
+                // Quitamos la clase active de alguna de otras tabs que la tengan.
+                if(this.tabs.querySelector('.tab--active')){
+                    this.tabs.querySelector('.tab--active').classList.remove('tab--active');
+                }
+
+                // Quitamos la clase active de alguna de otros botones que la tengan.
+                if(this.tabs.querySelector('.tabs__button--active')){
+                    this.tabs.querySelector('.tabs__button--active').classList.remove('tabs__button--active');
+                }
+
+                // Agregamos la clase active al tab.
+                this.tabs.querySelector(`#${tab}`).classList.add('tab--active');
+                
+                // Agregamos la clase active al boton.
+                e.target.classList.add('tabs__button--active');
+            }
+            
+        });
+    }
+}
+
+new Tabs('mas-informacion');
