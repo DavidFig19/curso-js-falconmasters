dfefccb2c7da6570666103136146cb064031409d
josue <josuefig19@gmail.com>
2026-01-07 21:12:44 -0600

    Agrega validacion filtro pelicula

--- Proyectos/Peliculas/public/bundle.js
+++ Proyectos/Peliculas/public/bundle.js
@@ -101,10 +101,22 @@ const cargarGeneros = async (filtro) => {
 const filtroPelicula = document.getElementById('movie');
 const filtroShow = document.getElementById('tv');
 
-filtroPelicula.addEventListener('click', (e) => {
+filtroPelicula.addEventListener('click', async (e) => {
     e.preventDefault();
-    console.log('Cargando Peliculas..');
-    
+    // Cargamos los generos en la barra lateral
+    cargarGeneros('movie');    
+
+    // Obtenemos los resultados
+    const resultados = await fetchPopulares('movie');
+
+    // Los cargamos en el DOM
+    cargarTitulos(resultados);
+
+    filtroShow.classList.remove('btn--active');
+    filtroPelicula.classList.add('btn--active');
+
+    document.querySelector('#populares .main__titulo').innerText = 'Películas Populares';
+
 });
 
 filtroShow.addEventListener('click', async (e) => {
@@ -114,8 +126,14 @@ filtroShow.addEventListener('click', async (e) => {
 
     // Obtenemos los resultados
     const resultados = await fetchPopulares('tv');
+
+    // Los cargamos en el DOM
     cargarTitulos(resultados);
-    
+
+    filtroPelicula.classList.remove('btn--active');
+    filtroShow.classList.add('btn--active');
+
+    document.querySelector('#populares .main__titulo').innerText = 'Series Populares';
 });
 
 const cargar = async () => {
--- Proyectos/Peliculas/src/listenerFiltroTipo.js
+++ Proyectos/Peliculas/src/listenerFiltroTipo.js
@@ -5,10 +5,22 @@ import fetchPopulares from "./fetchPopulares";
 const filtroPelicula = document.getElementById('movie');
 const filtroShow = document.getElementById('tv');
 
-filtroPelicula.addEventListener('click', (e) => {
+filtroPelicula.addEventListener('click', async (e) => {
     e.preventDefault();
-    console.log('Cargando Peliculas..');
-    
+    // Cargamos los generos en la barra lateral
+    cargarGeneros('movie');    
+
+    // Obtenemos los resultados
+    const resultados = await fetchPopulares('movie');
+
+    // Los cargamos en el DOM
+    cargarTitulos(resultados);
+
+    filtroShow.classList.remove('btn--active');
+    filtroPelicula.classList.add('btn--active');
+
+    document.querySelector('#populares .main__titulo').innerText = 'Películas Populares';
+
 });
 
 filtroShow.addEventListener('click', async (e) => {
@@ -18,6 +30,12 @@ filtroShow.addEventListener('click', async (e) => {
 
     // Obtenemos los resultados
     const resultados = await fetchPopulares('tv');
+
+    // Los cargamos en el DOM
     cargarTitulos(resultados);
-    
+
+    filtroPelicula.classList.remove('btn--active');
+    filtroShow.classList.add('btn--active');
+
+    document.querySelector('#populares .main__titulo').innerText = 'Series Populares';
 });
